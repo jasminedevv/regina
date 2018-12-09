0efc62287cf695b548d3a8ed1bae43fc0463fb23
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2018-12-09 14:54:20 -0800

    [deploy] please

--- regina/regina/settings.py
+++ regina/regina/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import djongo
+import django_heroku
+# import djongo
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
 ]
+django_heroku.settings(locals())
---
+++ regina/regina/settings_prod.py
@@ -0,0 +1,24 @@
+"""
+Settings unique to a production environment
+
+Using MySQL on Passenger
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/2.1/ref/settings/
+"""
+
+import os
+# do it like this because sometimes passenger crashes and reveals your environment variables
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['regina.eepy.net']
+
+DATABASES = db_options
+
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
+
+# this will probably be /public or something so that it can get static files at /public/static
+# STATIC_ROOT = 'some/path/idk'
